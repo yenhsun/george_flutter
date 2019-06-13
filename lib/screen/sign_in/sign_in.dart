e128a2c547cea069e5427058366fff31d2156e8f
yenhsun <yenhsun.huang@tinklabs.com>
2019-06-14 01:01:40 +0800

    test sign in 2

--- lib/screen/sign_in/sign_in.dart
+++ lib/screen/sign_in/sign_in.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:george_flutter/screen/sign_in/util/auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:toast/toast.dart';
 
 class SignInScreen extends StatelessWidget {
   @override
@@ -7,12 +11,78 @@ class SignInScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Sign in'),
       ),
-      body: Center(child: GoogleSignInButton()),
+      body: Center(child: GoogleSignInButton2()),
+    );
+  }
+}
+
+class GoogleSignInButton2 extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return SignInState();
+  }
+}
+
+class SignInState extends State<GoogleSignInButton2> {
+  final auth =
+  Auth(firebaseAuth: FirebaseAuth.instance, googleSignIn: GoogleSignIn());
+
+  Future<FirebaseUser> firebaseUser;
+
+  void _signIn() {
+    setState(() {
+      firebaseUser = auth.signInWithGoogle();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: firebaseUser,
+      builder: (context, AsyncSnapshot<FirebaseUser> snapshot) {
+        if (snapshot.hasData) {
+          if (snapshot.data.displayName.isNotEmpty) {
+            Toast.show("Hiiii ${snapshot.data.displayName}", context,
+                duration: Toast.LENGTH_SHORT, gravity: Toast.BOTTOM);
+            return Text("QQQQ");
+          } else {
+            Toast.show("Failed to get user display name", context,
+                duration: Toast.LENGTH_SHORT, gravity: Toast.BOTTOM);
+            return Text("22222");
+          }
+        } else {
+          return RaisedButton(
+            textColor: Colors.white,
+            color: Color.fromARGB(0xff, 0x42, 0x85, 0xF4),
+            padding: EdgeInsets.fromLTRB(10, 4, 10, 4),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                new Image.asset(
+                  'assets/ic_google_white_24dp.png',
+                ),
+                Padding(padding: EdgeInsets.fromLTRB(2, 0, 6, 0)),
+                Text(
+                  'Sign in with Google',
+                  textAlign: TextAlign.center,
+                )
+              ],
+            ),
+            onPressed: () {
+              _signIn();
+            },
+          );
+        }
+      },
     );
   }
 }
 
 class GoogleSignInButton extends StatelessWidget {
+  final auth =
+  Auth(firebaseAuth: FirebaseAuth.instance, googleSignIn: GoogleSignIn());
+
   @override
   Widget build(BuildContext context) {
     return RaisedButton(
@@ -21,16 +91,12 @@ class GoogleSignInButton extends StatelessWidget {
       padding: EdgeInsets.fromLTRB(10, 4, 10, 4),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
-//        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         mainAxisSize: MainAxisSize.min,
-//        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           new Image.asset(
             'assets/ic_google_white_24dp.png',
           ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(2, 0, 6, 0)
-          ),
+          Padding(padding: EdgeInsets.fromLTRB(2, 0, 6, 0)),
           Text(
             'Sign in with Google',
             textAlign: TextAlign.center,
@@ -38,8 +104,14 @@ class GoogleSignInButton extends StatelessWidget {
         ],
       ),
       onPressed: () {
-        // Navigate to the second screen using a named route.
-        debugPrint("QQQQQQ");
+        auth.signInWithGoogle().then((FirebaseUser user) {
+          if (user.displayName.isNotEmpty) {
+            Toast.show("Hi ${user.displayName}", context,
+                duration: Toast.LENGTH_SHORT, gravity: Toast.BOTTOM);
+          }
+        }).catchError((error) {
+          debugPrint('error: $error');
+        });
       },
     );
   }
--- lib/screen/sign_in/util/auth.dart
+++ lib/screen/sign_in/util/auth.dart
@@ -1,22 +1,25 @@
+import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-Future<GoogleSignInAccount> getSignedInAccount(
-    GoogleSignIn googleSignIn) async {
-  GoogleSignInAccount account = googleSignIn.currentUser;
-  if (account == null) {
-    account = await googleSignIn.signInSilently();
-  }
-  return account;
-}
+class Auth {
+  Auth({
+    @required this.googleSignIn,
+    @required this.firebaseAuth,
+  });
 
-Future<FirebaseUser> signIntoFirebase(
-    GoogleSignInAccount googleSignInAccount) async {
-  FirebaseAuth _auth = FirebaseAuth.instance;
-  GoogleSignInAuthentication googleAuth =
-      await googleSignInAccount.authentication;
-  return await _auth.signInWithGoogle(
-    accessToken: googleAuth.accessToken,
-    idToken: googleAuth.idToken,
-  );
+  final GoogleSignIn googleSignIn;
+  final FirebaseAuth firebaseAuth;
+
+  Future<FirebaseUser> signInWithGoogle() async {
+    final GoogleSignInAccount googleAccount = await googleSignIn.signIn();
+    debugPrint("googleAccount: $googleAccount");
+    final GoogleSignInAuthentication googleAuth =
+    await googleAccount.authentication;
+    debugPrint("googleAuth: $googleAuth");
+    return firebaseAuth.signInWithGoogle(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+  }
 }
