203ae99fbba511357336746213327a3cfa331d79
yenhsun <yenhsun.huang@tinklabs.com>
2019-06-15 05:21:58 +0800

    apply crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'screen/route_paths.dart';
 import 'package:george_flutter/screen/sign_in/sign_in.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 Future main() async {
+  Crashlytics.instance.enableInDevMode = true;
+
+  FlutterError.onError = (FlutterErrorDetails details) {
+    Crashlytics.instance.onError(details);
+  };
+
   runApp(MaterialApp(
     title: "Sign in",
     debugShowCheckedModeBanner: false,
